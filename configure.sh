455890ab5ed3f576fe6d4eced09931558b8645ec
Renata Desktop Ubuntu <renata.garcia.eng@gmail.com>
2020-02-16 17:07:48 -0300

    creating pycharm project

---
+++ configure.sh
@@ -0,0 +1,10 @@
+!/usr/bin/bash
+
+mkdir lib
+cp -r projects/grl/addons/python/share/grlenv/ lib/
+cp -r projects/grl/build/libgrl.so lib/
+cp -r projects/grl/build/libyaml-cpp.so.0.5 lib/
+cp -r projects/grl/build/grlpy.cpython-36m-x86_64-linux-gnu.so lib/
+cp -r projects/grl/build/libyaml-cpp.so.0.5.3 lib/
+
+
