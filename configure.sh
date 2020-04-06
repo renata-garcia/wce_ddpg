4e6c9167021973e6bdc14d562363d24b12ad55dc
Renata LCIP05 <renata.garcia.eng@gmail.com>
2020-04-05 22:25:52 -0300

    lib files need to be in src

--- configure.sh
+++ configure.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-mkdir lib
-cp -r ~/projects/grl/addons/python/share/grlenv/ lib/
-cp -r ~/projects/grl/build/libgrl.so lib/
-cp --remove-destination ~/projects/grl/build/libyaml-cpp.so.0.5 lib/
-cp -r ~/projects/grl/build/grlpy.cpython-36m-x86_64-linux-gnu.so lib/
-#cp -r ~/projects/grl/build/libyaml-cpp.so.0.5.3 lib/
+#mkdir lib
+cp -r ~/projects/grl/addons/python/share/grlenv/ .
+cp -r ~/projects/grl/build/libgrl.so .
+cp --remove-destination ~/projects/grl/build/libyaml-cpp.so.0.5 .
+cp -r ~/projects/grl/build/grlpy.cpython-36m-x86_64-linux-gnu.so .
+#cp -r ~/projects/grl/build/libyaml-cpp.so.0.5.3 .
 
 
